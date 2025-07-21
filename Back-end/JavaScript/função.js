4880185da41a294d3fd2b2c3fda380c6ddfb766c
Cassio <97923417+cassio1039@users.noreply.github.com>
2025-07-21 00:05:33 -0300

    atualizando

--- Back-end/JavaScript/função.js
+++ Back-end/JavaScript/função.js
@@ -1,5 +1,19 @@
-function somar(x1, x2){
-    return x1 + x2;
+function calcular(x1, x2, operador) {
+  return eval(`${x1} ${operador} ${x2}`);
 }
-console.log(somar(1,6));
+let resultado = calcular(1, 2, "*");
+console.log(resultado);
+
+(function calcular(x1, x2, operador) {
+  return eval(`${x1} ${operador} ${x2}`);
+})(5,5, "*")
+
+console.log();
+
+/* 
+eval = pega uma string e interpreta ela
+Para chamar uma função anônima, coloque-se parentêses antes da função e também assim que terminar ela 
+*/
+
+
 
