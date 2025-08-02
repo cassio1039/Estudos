903c83fc0f4684af395d60cad7e8cad793fb59d8
Cassio <97923417+cassio1039@users.noreply.github.com>
2025-08-02 03:32:17 -0300

    estudos

--- Projetos/React/projeto-pomodoro/eslint.config.js
+++ Projetos/React/projeto-pomodoro/eslint.config.js
@@ -1,23 +1,20 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default [
+  ...tseslint.configs.recommended,
+  js.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    ignores: ['dist'],
     languageOptions: {
       ecmaVersion: 2020,
+      sourceType: 'module',
       globals: globals.browser,
     },
+    rules: {
+      semi: ['error', 'always'],
+    },
   },
-])
+]
