3edd89bd3d476155869867017b05a2112102a2ba
Cassio <cassiocmm@outlook.com>
2026-09-13 08:15:25 -0300

    finalizando tabela de preço

--- Back-end/Java/Tabela de preco/src/Main.java
+++ Back-end/Java/Tabela de preco/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     static void main(String[] args) {
-
+        Scanner scanner =  new Scanner(System.in);
+        System.out.println("Insira quantos produtos você quer: ");
+        int produto = scanner.nextInt();
+        System.out.println("Qual o preço do produto?");
+        double preco = scanner.nextFloat();
+        double total = produto * preco;
+        System.out.println("O valor total a pagar do produto é " + String.format("%.2f", total));
     }
 }
